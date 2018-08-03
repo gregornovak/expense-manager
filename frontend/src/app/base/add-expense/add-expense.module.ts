9917c4aaca27c04ee8a54071746f4628aa1aa904
gregornovak <grega_novak@yahoo.com>
2018-08-03 22:54:58 +0200

    Added all fields to add expense form, and added some styles

--- frontend/src/app/base/add-expense/add-expense.module.ts
+++ frontend/src/app/base/add-expense/add-expense.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
+import { MatSlideToggleModule, MatInputModule } from '@angular/material';
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatInputModule,
+        MatSlideToggleModule
+    ],
+    exports: [
+        MatInputModule,
+        MatSlideToggleModule
+    ]
 })
 
 export class AddExpenseModule { }
