324f5794d68ec01210f7a5a9d5e752c5eef8dc9c
gregornovak <grega_novak@yahoo.com>
2018-08-23 22:23:36 +0200

    Added php-cs-fixer and tried it on one file

--- src/Controller/ExpenseController.php
+++ src/Controller/ExpenseController.php
@@ -36,8 +36,7 @@ class ExpenseController extends Controller
         JwtAuthenticator $authenticator,
         UserProviderInterface $userProvider,
         PaginatorChecker $paginatorChecker
-    )
-    {
+    ) {
         $this->serializer = $serializer;
         $this->validator = $validator;
         $this->authenticator = $authenticator;
@@ -48,7 +47,9 @@ class ExpenseController extends Controller
     /**
      * @Route("/expenses", name="get_expenses_collection")
      * @Method("GET")
+     *
      * @param Request $request
+     *
      * @return JsonResponse
      */
     public function index(Request $request): JsonResponse
@@ -63,11 +64,11 @@ class ExpenseController extends Controller
 
         $repository = $this->getDoctrine()->getRepository(Expenses::class);
 
-        if(is_numeric($page) &&
+        if (is_numeric($page) &&
             is_numeric($limit) &&
             $this->paginatorChecker->isWithinRange($page, $limit)
         ) {
-            $results = $repository->getAll($user->getId(), (int)$page, (int)$limit);
+            $results = $repository->getAll($user->getId(), (int) $page, (int) $limit);
         } else {
             $results = $repository->getAll($user->getId());
         }
@@ -90,9 +91,12 @@ class ExpenseController extends Controller
     /**
      * @Route("/expenses/{id}", name="get_expense")
      * @Method("GET")
+     *
      * @param Request $request
-     * @param string $id
+     * @param string  $id
+     *
      * @return JsonResponse
+     *
      * @throws HttpException
      */
     public function getExpense(Request $request, string $id): JsonResponse
@@ -105,12 +109,12 @@ class ExpenseController extends Controller
         $em = $this->getDoctrine()->getManager();
         $expense = $em->getRepository(Expenses::class)->find($id);
 
-        if(!$expense) {
-            throw new HttpException(404,'Resource does not exist.');
+        if (!$expense) {
+            throw new HttpException(404, 'Resource does not exist.');
         }
 
-        if($authenticatedUser->getId() != $expense->getUser()->getId()) {
-            throw new HttpException(400,'You do not have permission to view this resource.');
+        if ($authenticatedUser->getId() != $expense->getUser()->getId()) {
+            throw new HttpException(400, 'You do not have permission to view this resource.');
         }
 
         $response = $this->serializer->serialize(['success' => true, 'data' => $expense],
@@ -126,8 +130,11 @@ class ExpenseController extends Controller
     /**
      * @Route("/expenses", name="post_expenses")
      * @Method("POST")
+     *
      * @param Request $request
+     *
      * @return JsonResponse
+     *
      * @throws HttpException
      */
     public function save(Request $request): JsonResponse
@@ -139,23 +146,22 @@ class ExpenseController extends Controller
 
         $related = json_decode($request->getContent());
 
-        if(!isset($related->expenses_category) || empty($related->expenses_category)
-        )
-        {
+        if (!isset($related->expenses_category) || empty($related->expenses_category)
+        ) {
             throw new HttpException(400, 'You must set expenses_category id');
         }
 
         $em = $this->getDoctrine()->getManager();
         $user = $em->getRepository(User::class)->find($authenticatedUser->getId());
 
-        if(!$user) {
+        if (!$user) {
             throw new HttpException(404, 'This user does not exist!');
         }
 
         $category = $em->getRepository(ExpensesCategories::class)
             ->findCategoryByUser($user->getId(), $related->expenses_category)[0];
 
-        if(!$category) {
+        if (!$category) {
             throw new HttpException(404, 'This expenses category does not exist!');
         }
 
@@ -169,7 +175,7 @@ class ExpenseController extends Controller
 
         $errors = $this->validator->validate($data);
 
-        if(count($errors) > 0) {
+        if (count($errors) > 0) {
             throw new HttpException(400, 'You must provide all required properties.');
         }
 
@@ -184,17 +190,17 @@ class ExpenseController extends Controller
         $expense->setAmount($data->getAmount());
         $expense->setCurrency($data->getCurrency());
 
-        if($data->getCash()) {
+        if ($data->getCash()) {
             $expense->setCash($data->getCash());
         }
 
-        $expense->setPayee($data->getPayee() ?? "");
+        $expense->setPayee($data->getPayee() ?? '');
 
-        if($data->getStatus()) {
+        if ($data->getStatus()) {
             $expense->setStatus($data->getStatus());
         }
 
-        if($data->getDescription()) {
+        if ($data->getDescription()) {
             $expense->setDescription($data->getDescription());
         }
 
@@ -215,9 +221,12 @@ class ExpenseController extends Controller
     /**
      * @Route("/expenses/{id}", name="update_expense")
      * @Method("PUT")
+     *
      * @param Request $request
-     * @param string $id
+     * @param string  $id
+     *
      * @return JsonResponse
+     *
      * @throws HttpException
      */
     public function update(Request $request, string $id): JsonResponse
@@ -232,15 +241,15 @@ class ExpenseController extends Controller
         $em = $this->getDoctrine()->getManager();
         $user = $em->getRepository(User::class)->find($authenticatedUser->getId());
 
-        if(!$user) {
+        if (!$user) {
             throw new HttpException(404, 'This user does not exist!');
         }
 
-        if(isset($related->expenses_category) && !empty($related->expenses_category)) {
+        if (isset($related->expenses_category) && !empty($related->expenses_category)) {
             $category = $em->getRepository(ExpensesCategories::class)
                 ->findCategoryByUser($user->getId(), $related->expenses_category)[0];
 
-            if(!$category) {
+            if (!$category) {
                 throw new HttpException(404, 'This expenses category does not exist!');
             }
         }
@@ -255,21 +264,21 @@ class ExpenseController extends Controller
 
         $errors = $this->validator->validate($data);
 
-        if(count($errors) > 0) {
+        if (count($errors) > 0) {
             throw new HttpException(400, 'You must provide all required properties.');
         }
 
         $expense = $em->getRepository(Expenses::class)->find($id);
 
-        if(!$expense) {
+        if (!$expense) {
             throw new HttpException(404, 'No resource found.');
         }
 
-        if($expense->getUser()->getId() != $authenticatedUser->getId()) {
-            throw new HttpException(400,'You do not have permission to update this resource.');
+        if ($expense->getUser()->getId() != $authenticatedUser->getId()) {
+            throw new HttpException(400, 'You do not have permission to update this resource.');
         }
 
-        if(isset($category)) {
+        if (isset($category)) {
             $expense->setExpensesCategory($category);
         }
         $expense->setName($data->getName());
@@ -277,19 +286,19 @@ class ExpenseController extends Controller
         $expense->setAmount($data->getAmount());
         $expense->setCurrency($data->getCurrency());
 
-        if($data->getCash() && gettype($data->getCash()) === "boolean") {
+        if ($data->getCash() && 'boolean' === gettype($data->getCash())) {
             $expense->setCash($data->getCash());
         }
 
-        if($data->getPayee()) {
+        if ($data->getPayee()) {
             $expense->setPayee($data->getPayee());
         }
 
-        if($data->getStatus()) {
+        if ($data->getStatus()) {
             $expense->setStatus($data->getStatus());
         }
 
-        if($data->getDescription()) {
+        if ($data->getDescription()) {
             $expense->setDescription($data->getDescription());
         }
 
@@ -309,9 +318,12 @@ class ExpenseController extends Controller
     /**
      * @Route("/expenses/{id}", name="delete_expense")
      * @Method("DELETE")
+     *
      * @param Request $request
-     * @param string $id
+     * @param string  $id
+     *
      * @return JsonResponse
+     *
      * @throws HttpException
      */
     public function delete(Request $request, string $id): JsonResponse
@@ -324,17 +336,17 @@ class ExpenseController extends Controller
         $em = $this->getDoctrine()->getManager();
         $user = $em->getRepository(User::class)->find($authenticatedUser->getId());
 
-        if(!$user) {
+        if (!$user) {
             throw new HttpException(404, 'This user does not exist!');
         }
 
         $expense = $em->getRepository(Expenses::class)->find($id);
 
-        if(!$expense) {
+        if (!$expense) {
             throw new HttpException(404, 'Resource not found.');
         }
 
-        if($expense->getUser()->getId() != $authenticatedUser->getId()) {
+        if ($expense->getUser()->getId() != $authenticatedUser->getId()) {
             throw new HttpException(400, 'You do not have permission to delete expense as somebody else!');
         }
 
@@ -347,9 +359,11 @@ class ExpenseController extends Controller
     /**
      * @Route("/monthly-expenses/{month}/{year}", name="get_expenses_by_month", defaults={"year"=null})
      * @Method("GET")
+     *
      * @param Request $request
-     * @param string $month
-     * @param string $year
+     * @param string  $month
+     * @param string  $year
+     *
      * @return JsonResponse
      */
     public function getByMonth(Request $request, string $month, ?string $year): JsonResponse
@@ -364,10 +378,10 @@ class ExpenseController extends Controller
 
         $repository = $this->getDoctrine()->getRepository(Expenses::class);
 
-        if(!$page || !is_numeric($page) || !$limit || !is_numeric($limit)) {
+        if (!$page || !is_numeric($page) || !$limit || !is_numeric($limit)) {
             $results = $repository->getExpensesByMonth($user->getId(), $month, $year);
         } else {
-            $results = $repository->getExpensesByMonth($user->getId(), $month, $year, (int)$page, (int)$limit);
+            $results = $repository->getExpensesByMonth($user->getId(), $month, $year, (int) $page, (int) $limit);
         }
 
         if (!$results) {
